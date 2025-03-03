ad95292e36acd0775148ea7ab0a911a8e9855e73
Christopher Rodriguez <148895438+christopfa9@users.noreply.github.com>
2025-03-03 15:30:11 -0600

    APIs de Backend
    tiene las APIs para que se pueda hacer login, register y logout, utilizando JWT y cookies

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,64 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js";
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      name,
+      email,
+      password: passwordHash,
+    });
+
+    const userSaved = await newUser.save();
+    const token = await createAccessToken({ id: userSaved._id });
+
+    res.cookie("token", token);
+
+    res.json({
+      id: userSaved._id,
+      name: userSaved.name,
+      email: userSaved.email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const UserFound = await User.findOne({email});
+
+    if (!UserFound) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, UserFound.password);
+
+    if (!isMatch) return res.status(400).json({ message: "Incorrect password" });
+
+    const token = await createAccessToken({ id: UserFound._id });
+
+    res.cookie("token", token);
+
+    res.json({
+      id: UserFound._id,
+      name: UserFound.name,
+      email: UserFound.email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = (req, res) => {
+
+  res.cookie("token", "" ,{
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
+}
