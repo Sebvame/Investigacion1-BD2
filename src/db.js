23f5a456a41ac55c3937911263726fe93c0abb32
Sebvame <111831573+Sebvame@users.noreply.github.com>
2025-03-05 20:24:14 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
 
     try {
-        await mongoose.connect("mongodb+srv://christopfa9:DWWvSdFn7A9RsAcl@tarea1.jyfc2.mongodb.net/?retryWrites=true&w=majority&appName=tarea1");
-        console.log("Database connected");
+        await mongoose.connect(process.env.MONGODB_URI);
     } catch (error) {
         console.log("Error connecting to database", error);
     }
