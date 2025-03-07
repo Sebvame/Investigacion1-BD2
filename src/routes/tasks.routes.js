e02d611b0bcc9438b18f443d186cfb58878a32c3
Christopher Rodriguez <148895438+christopfa9@users.noreply.github.com>
2025-03-07 15:53:44 -0600

    jeje, se me olvido probarlo antes de subirlo
    le arregle unas rutas que estaban mal, ya tiene una parte del frontend

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {authRequired} from "../middlewares/validateToken.js";
-import {getTasks, getTask, createTask, deleteTask, updateTask} from "../controllers/tasks.controller.js";
+import {getTasks, getTask, createTask, deleteTask, UpdateTask} from "../controllers/task.controller.js";
 import { validaShema } from "../middlewares/validator.middleware.js";
 import { createTaskSchema } from "../schemas/task.schema.js";
 
@@ -8,8 +8,8 @@ const router = Router();
 
 router.get("/tasks", authRequired, getTasks);
 router.get("/tasks:id", authRequired, getTask);
-router.post("/tasks", authRequired, validaShema(createTask), createTask);
+router.post("/tasks", authRequired, validaShema(createTaskSchema), createTask);
 router.delete("/tasks:id", authRequired, deleteTask);
-router.put("/tasks:id", authRequired, updateTask);
+router.put("/tasks:id", authRequired, UpdateTask);
 
 export default router;
