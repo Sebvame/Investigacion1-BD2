5228b984d57ef5273367f8e39c04538567c5665c
Christopher Rodriguez <148895438+christopfa9@users.noreply.github.com>
2025-03-07 18:27:39 -0600

    No dio tiempo de hacer el front end
    :(

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,6 +6,10 @@ export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
+
+    const userFound = await User.findOne({ email });
+    if(userFound) return res.status(400).json({message: ["The email is already in use"]});
+
     const passwordHash = await bcrypt.hash(password, 10);
 
     const newUser = new User({
